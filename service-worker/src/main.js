0ea9fc431b6acbd55754e4e69478772a1631fff5
ryantbwarren <ryan.tb.warren@gmail.com>
2021-05-01 16:53:41 +0100

    initial service worker example commit

---
+++ service-worker/src/main.js
@@ -0,0 +1,16 @@
+(() => {
+    console.log("[main.js] hello world");
+
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+            .register("sw.js")
+            .then((reg) => {
+                // registration worked
+                console.log("Registration succeeded. Scope is " + reg.scope);
+            })
+            .catch((error) => {
+                // registration failed
+                console.log("Registration failed with " + error);
+            });
+    }
+})();
